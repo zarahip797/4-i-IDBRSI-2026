7afd2a5b1c58e412b00bb0bda962fd903fad638d
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-27 15:13:02 -0600

    Add files via upload

---
+++ trabajo 2.py
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+class salary
+
+def salary(self):
+public int id 
+public int salary 
+
+public salary () {
+    id = int,
+    salary = int;
+
+public(id, salary self):
+
+public striing Obtener resultado()
+    { return $"{id}, [salary]"
+}
+}
