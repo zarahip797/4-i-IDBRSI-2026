7cd843a19b6a926b595c1869d65fa3a149632fd7
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-27 15:30:55 -0600

    Add files via upload

---
+++ trabajo 4.py
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+class public 
+def trips(db.model):
+    id = db.column(integer, primary-key)
+    client = db.column(nullabe=false)
+    driver = db.colun(nullabe=false)
+    city = db.column(nullabe=false)
+
+def users(db.model)
+    users_id = db.column(integer, primary-key)
+    banned_id = db.column(nullabl=false)
+    role = db.column(nullable=false)
