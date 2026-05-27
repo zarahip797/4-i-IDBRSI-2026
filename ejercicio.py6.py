3f738263b31a78d2316f68dbc175755bdcefe713
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-26 19:09:23 -0600

    Add files via upload

---
+++ ejercicio.py6.py
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+class customer import user
+
+def activity(self):
+    customer = int 
+    product = int 
+
+public int customer = customer 
+    public = int 
+    public = int 
+
+    public string obtener resultado()
+    return $('customer') ('product')
