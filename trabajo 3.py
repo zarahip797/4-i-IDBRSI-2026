9f21320ff52f40fda77a1ab8fa54c55e1b541c3a
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-27 15:21:41 -0600

    Add files via upload

---
+++ trabajo 3.py
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+class departament 
+    def twosun (self, nums: List[int], employed: int) List[int])
+
+    public int id = id
+    public varchar employee = employee 
+    public int salary = salary
+    public varchar dapartment = department 
+
+    public string obtener resultado()
+    return ${´id´} {´department´},{´salary´},{´employee´}
+
+
+pass
