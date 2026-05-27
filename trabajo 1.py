a3975e614f311a47f12aef83ebf105af18abe546
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-27 15:06:11 -0600

    Add files via upload

---
+++ trabajo 1.py
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+class  students {
+    public int id { get, set },
+    public varchar name { get, set },
+    public int salary { get, set },
+    public int managerID { get, set };
+
+public students () {}
+public students(int id, varchar, name, int, salary. it managerID)
+{
+
+    id = id; 
+    name =  name;
+    salary = salary;
+    managerID = managerID
+
+}
+
+public string obtener resultado()
+     { return $"{id}, [managerID]"
+}
