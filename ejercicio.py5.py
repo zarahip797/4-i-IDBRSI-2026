a39baee6f3e6792c40c46d85a638757ec4098194
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-26 19:18:20 -0600

    Add files via upload

---
+++ ejercicio.py5.py
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+class player import user
+
+def activity(self):
+    player = int 
+    device = int
+    event_date = date
+    games_played = int
+
+public int player = player 
+    public = int
+    public = int 
+    public = date 
+    public = int
+
+    public string obtener resultado ()
+    return $('player') ('device'), ('event'), ('games_played')
