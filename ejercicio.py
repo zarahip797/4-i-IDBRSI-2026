5a8228fa7c9e6c8b15c96ee541ca8f0e358151d4
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-26 19:02:33 -0600

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+class public import public 
+def table(self)
+trasaction_id = int 
+amount = int 
+transaction_date = date
+
+def transaction(self):
+odd_sum = input(int)
+trasaction_date = input(varchar)
+
+public transaction(self):
+    trasaction_id = int
+    amount = int
+    trasaction_date = date 
+    return app ({'trasaction_id'}, {'amount'}, {'trasaction_date'})
