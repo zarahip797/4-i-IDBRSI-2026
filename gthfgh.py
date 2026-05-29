550ed2acb6996fdb63038d2e60173c02e6fed529
zarahip797 <hernandez.paloma.2024@cbtis-246.edu.mx>
2026-05-29 17:12:12 -0600

    Add files via upload

---
+++ gthfgh.py
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+create table stadium (
+id int auto_increrment NOT NULL
+visit_date DATE NOT NULL,
+people int NOT NULL,
+);
+
+insert into stadium(id, visit_date, people)
+VALUES(1, '2017-01-01', 10),
+(2, '20217-01-02', 109),
+(3. '2017-01-03' , 150)
+(4, '2017-01-04', 99),
+(5, '2017-01-05', 145),
+(6, '2017-01-06', 1455)
+(7, '2017-01-07', 199)
+(8, '20217-01-08', 188)
+
+SELECT 5, As '2017-01-05' AS 10 
+UNION ALL 
+6, '2017-01-06', 1455
+UNION ALL 
+7, '20217-01-07', 199
+UNION ALL 
+8, '2017 -01-08', 188
